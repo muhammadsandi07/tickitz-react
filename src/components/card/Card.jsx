af453e749ded58c5dc17c515bc0343ed6d419fc7
muhammad sandi <muhammadsandimaulana07@gmail.com>
2025-03-24 15:14:10 +0700

    feat: update component card movie

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,12 +1,17 @@
 import React from "react"
 import { Link } from "react-router"
+import noimage from "../../assets/other/noimage.jpg"
 
 const Card = ({ title, poster_path, id, genre }) => {
   return (
-    <div>
+    <div className='w-[240px] max-sm:mx-auto'>
       <div className=' w-[240px] h-[369px] relative group rounded-md '>
         <img
-          src={`https://image.tmdb.org/t/p/original${poster_path})] `}
+          src={
+            poster_path
+              ? `https://image.tmdb.org/t/p/original${poster_path}) `
+              : noimage
+          }
           alt=''
           className='w-full h-full rounded-md'
         />
@@ -22,11 +27,11 @@ const Card = ({ title, poster_path, id, genre }) => {
         </div>
       </div>
       <div>
-        <h5 className='text-lg font-bold text-[#14142B] leading-[34px] tracking-[1px] my-[19px] w-[240px] h-[68px]'>
+        <h5 className='text-lg font-bold text-[#14142B] leading-[34px] tracking-[1px] max-sm:my-[5px] my-[19px] w-[240px] h-max'>
           {title}
         </h5>
         <div className='flex w-[240px] flex-wrap gap-2'>
-          {genre?.map((genre, id) => (
+          {genre.map((genre, id) => (
             <p
               key={id}
               className='leading-8 tracking-[0.75px] text-light-text px-[8px] py-[2px] bg-[#A0A3BD1A] rounded-[20px]'

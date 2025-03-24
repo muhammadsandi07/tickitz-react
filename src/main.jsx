8e14484ba0f32920a21385ee0f2190b53ac6b79c
muhammad sandi <muhammadsandimaulana07@gmail.com>
2025-03-24 15:43:02 +0700

    feat: add redux provider and browserRouting

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App.jsx"
+import { BrowserRouter } from "react-router"
+import UserProvider from "./contexts/userContext.jsx"
+import ReduxProvider from "./redux/reduxProviders.jsx"
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ReduxProvider>
+      <UserProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </UserProvider>
+    </ReduxProvider>
+  </StrictMode>
 )

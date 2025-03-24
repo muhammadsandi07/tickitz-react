f48c8e4b1505a4c1cfddb9fcbf53af5b6ffdf31c
muhammad sandi <muhammadsandimaulana07@gmail.com>
2025-03-24 15:11:16 +0700

    api: update api movieDetails

--- src/api/apiMovieDetails.js
+++ src/api/apiMovieDetails.js
@@ -1,3 +1,4 @@
+import formatterDate from "../utils/formatterDate.js"
 import getGenre from "./apiGenre.js"
 
 const API_KEY = import.meta.env.VITE_API_KEY
@@ -24,6 +25,7 @@ const getMovieDetails = async (id) => {
       cast: "",
       genre: [],
       director: "",
+      synopsis: "",
     }
     const [responseDetails, responseCredits] = await Promise.all([
       fetch(`https://api.themoviedb.org/3/movie/${id}`, options),
@@ -32,28 +34,43 @@ const getMovieDetails = async (id) => {
 
     const dataDetails = await responseDetails.json()
     const dataCredits = await responseCredits.json()
-    const { title, backdrop_path, poster_path, release_date, runtime, genres } =
-      dataDetails
+
+    const {
+      title,
+      backdrop_path,
+      poster_path,
+      release_date,
+      runtime,
+      genres,
+      overview,
+    } = dataDetails
+
+    const { date, month, year } = formatterDate(release_date)
+    dataMovie.releaseDate = `${month} ${date}, ${year}`
     dataMovie.title = title
     dataMovie.background = backdrop_path
     dataMovie.img = poster_path
     dataMovie.duration.hour = Math.floor(runtime / 60)
     dataMovie.duration.minute = runtime - dataMovie.duration.hour * 60
     dataMovie.genre = genres
-
+    dataMovie.synopsis = overview
+    const arrCast = []
     for (let index = 0; index < dataCredits.cast.length; index++) {
       if (index > 2) {
-        dataMovie.cast += dataCredits.crew[index].name + ", "
-        dataMovie.cast += "..."
+        arrCast.push(dataCredits.cast[index].name)
+
         break
+      } else {
+        arrCast.push(dataCredits.cast[index].name)
       }
-      dataMovie.cast += dataCredits.crew[index].name + ", "
     }
+    dataMovie.cast = arrCast.join(", ")
+
     dataMovie.director = dataCredits.crew
       .filter((pekerja) => pekerja.job === "Director")
       .map((pekerja) => pekerja.name)
       .join(", ")
-
+    console.log(dataMovie, "data movie")
     return dataMovie
   } catch (error) {
     console.log(error)
